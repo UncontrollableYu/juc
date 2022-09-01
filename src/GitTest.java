2c0c81057a26852faa90067a14ec1f223cceb336
UncontrollableYu <feng5408892@gmail.com>
2022-09-01 09:38:22 +0800

    four commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,8 +1,14 @@
+import static sun.misc.PostVMInitHook.run;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello feng");
         System.out.println("hello yu");
         System.out.println("hello git hot-fix");
+
+    }
+    public void fun(){
+
     }
 }
